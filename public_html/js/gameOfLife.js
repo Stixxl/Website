2f7e57e70fb7537327f1651e39d83b3f3acb7f3c
Corbinian <corbinho@hotmail.de>
2016-05-28 15:29:58 +0200

    significantly changed gameOfLife

--- public_html/js/gameOfLife.js
+++ public_html/js/gameOfLife.js
@@ -1,34 +1,46 @@
-var playboard = new Array(62); // create additional row left up and down to simmplify the check of neighbors
+var CELL_SIZE = 10;
+var WIDTH = 600;
+var HEIGHT = 600;
+var COUNT_X = WIDTH / CELL_SIZE;
+var COUNT_Y = HEIGHT / CELL_SIZE;
+var DELAY = 500;
 
-function playGame()
-{
-    for (var i = 0; i < 62; i++)
-{
-    playboard[i] = new Array(62); // same reasoning as above
-    for (var j = 0; j < 62; j++)
-    {
-        playboard[i][j] = false; // false == dead, true == alive
+var playboard = matrix(COUNT_X, COUNT_Y, 0);
+var newPlayboard = matrix(COUNT_X, COUNT_Y, 0);
+//counts generations
+var counter = 0;
+//start/stop functionality
+var isRunning = 0;
+var intervalID;
+
+var canvas = document.getElementById("drawingBoard");
+var canvasContext = canvas.getContext("2d");
+var button = document.getElementById("playButton");
+var generationSpan = document.getElementById("counter");
+
+button.onclick = function () {
+    switch (isRunning) {
+        case 0:
+            intervalID = setInterval(function () {
+                update();
+            }, DELAY);
+            isRunning = 1;
+            break;
+        default:
+            clearInterval(intervalID);
+            isRunning = 0;
     }
-}
-playboard[10][10] = true;
-playboard[10][11] = true;
-playboard[10][12] = true;
+};
 
-while(true)
-{
-
-    setTimeout(drawPlayboard(), 1000);
-}
-}
 
 function applyGameLogic()
 {
 
-    for (var i = 1; i < 61; i++)
+    for (var i = 0; i < COUNT_Y; i++)
     {
-        for (var j = 1; j < 61; j++)
+        for (var j = 0; j < COUNT_X; j++)
         {
-            if (playboard[i][j]) //cell is alive
+            if (playboard[i][j] === 1) //cell is alive
             {
                 handleLiveCell(i, j);
             } else
@@ -37,19 +49,21 @@ function applyGameLogic()
             }
         }
     }
+    counter++;
+    copyGrid(newPlayboard, playboard);
 }
 function handleLiveCell(x, y)
 {
     var count = countNeighbors(x, y);
     if (count < 2) // death by underpopulation
     {
-        playboard[x][y] = false;
+        newPlayboard[x][y] = 0;
     } else if (count < 4) // lives for another generation
     {
-        playboard[x][y] = true;
+        newPlayboard[x][y] = 1;
     } else // death by overpopulation
     {
-        playboard[x][y] = false;
+        newPlayboard[x][y] = 0;
     }
 }
 
@@ -58,20 +72,20 @@ function handleDeadCell(x, y)
     var count = countNeighbors(x, y);
     if (count === 3) // born by means of repopulation
     {
-        playboard[x][y] = true;
+        newPlayboard[x][y] = 1;
     } else //no birth
     {
-        playboard[x][y] = false;
+        newPlayboard[x][y] = 0;
     }
 }
-function countNeighbors(x, y)// returns the count of all neighbors that are true
+function countNeighbors(x, y)// returns the count of all neighbors that are alive
 {
     var count = 0;
     for (var i = x - 1; i < x + 2; i++)
     {
         for (var j = y - 1; j < y + 2; j++)
         {
-            if (playboard[i][j] && i !== x && j !== y)
+            if (playboard[(COUNT_Y + i) % COUNT_Y][(COUNT_X + j) % COUNT_X] === 1 && i !== x && j !== y)
             {
                 count++;
             }
@@ -79,27 +93,62 @@ function countNeighbors(x, y)// returns the count of all neighbors that are true
     }
     return count;
 }
-
+function update()
+{
+    applyGameLogic();
+    drawPlayboard();
+}
 function drawPlayboard()
 {
-    var canvas = document.getElementById("drawingBoard");
-    var canvasContext = canvas.getContext("2d");
-        applyGameLogic();
-    for(var i = 0; i < 60; i++)
+    for (var i = 0; i < 60; i++)
     {
-        for(var j = 0; j <60; j++)
+        for (var j = 0; j < 60; j++)
         {
-            canvasContext.arc(5 + 10 * i, 5 + 10 * j, 5, 0, 2* Math.PI);
-            if(playboard[i][j])
-            {
-                canvasContext.fill();
+            if (newPlayboard[i][j] === 1) {
+                canvasContext.fillStyle = "#000";
+            } else {
+                canvasContext.fillStyle = "#eee";
+                //context.clearRect();
             }
-            else
-            {
-                canvasContext.stroke();
-            }
+            canvasContext.fillRect(
+                    j * CELL_SIZE + 1,
+                    i * CELL_SIZE + 1,
+                    CELL_SIZE - 1,
+                    CELL_SIZE - 1);
         }
     }
-}
 
+    generationSpan.innerHTML = "current generation: " + counter;
+}
+for (var x = 0; x <= WIDTH; x += CELL_SIZE) {
+    canvasContext.moveTo(0.5 + x, 0);
+    canvasContext.lineTo(0.5 + x, HEIGHT);
+}
+for (var y = 0; y <= HEIGHT; y += CELL_SIZE) {
+    canvasContext.moveTo(0, 0.5 + y);
+    canvasContext.lineTo(WIDTH, 0.5 + y);
+}
+canvasContext.strokeStyle = "#fff";
+canvasContext.stroke();
+function copyGrid(source, destination) {
+    for (var h = 0; h < COUNT_Y; h++) {
+        /*
+         for (var w = 0; w < Life.WIDTH; w++) {
+         destination[h][w] = source[h][w];
+         }
+         */
+        destination[h] = source[h].slice(0);
+    }
+}
+function matrix(m, n, initial) {
+    var a, i, j, mat = [];
+    for (i = 0; i < m; i += 1) {
+        a = [];
+        for (j = 0; j < n; j += 1) {
+            a[j] = 0;
+        }
+        mat[i] = a;
+    }
+    return mat;
+}
 
